56b94f670c14f96045874dab93e771f96edcead9
Michael Juhasz <michael.juhasz@gmail.com>
2013-11-03 20:25:56 -0500

    Added the logo.  Also futzed around with bootstrap, which has messed certain things up.  Hopefully the fixes are forthcoming...

--- views/_v_template.php
+++ views/_v_template.php
@@ -17,37 +17,40 @@
 	<link href="/css/general.css" rel="stylesheet">
 </head>
 
-<body class='anti-bootstrap'>	
-	<div class='navbar navbar-inverse navbar-fixed-top' role ='navigation' id='menu'>
-		<div class='container'>
-			<ul class='nav navbar-nav'>
+<body>	
+	<div class='navbar navbar-inverse navbar-fixed-top' role ='navigation'>
+		<div class='navbar-header'>
+			<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#nvbr'>
+				<span class='sr-only'>Toggle navigation</span>
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+			</button>
+			<a class='navbar-brand' rel='home' href='/'><img src='/images/logoIcon50.png'></a>
+			<div class='collapse navbar-collapse' id='nvbr'>
 
-				<li><a href="/">Logo</a></li>
+				<ul class='nav navbar-nav'>
+					<!-- Menu for logged in users-->
+					<?php if($user): ?>
+						<li><a href='/users/profile'><?=$user->first_name;?></a></li>
+						<li><a href='/users/logout'>Logout</a></li>
 
-				<!-- Menu for logged in users-->
-				<?php if($user): ?>
-					<li><a href='/users/profile'><?=$user->first_name;?></a></li>
-					<li><a href='/users/logout'>Logout</a></li>
-
-				<?php endif; ?>
-
-				<li>
+					<?php endif; ?>
+				</ul>
+				<div class='col-md-3'>
 					<form id='searchbar' class='navbar-form' role='search' method='POST' action='/users/p_search'>
 						<div class='input-group'>
-							<input type='text' name='search' class='form-control' placeholder='name, bio, location...'>
+							<input type='text' name='search' class='form-control navbar-search' placeholder='name, bio, location...'>
 							<div class='input-group-btn'>
 								<button class='btn btn-default' type='submit'><i class='glyphicon glyphicon-search'></i></button>
 							</div>
 						</div>	
 					</form>
-				</li>
-			</ul>
+				</div>
+			</div>	
 		</div>
 	</div>
 	
-	<!--<?php for ($i = 1; $i < 5; $i++) {
-		if(isset(${$content.$i})) echo ${$content.$i}; 
-		};?>-->
 	<!-- <div class='container'>	 -->
 		<?php if(isset($content1)) echo $content1; ?>
 		<?php if(isset($content2)) echo $content2; ?>
--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,27 +1,26 @@
-<div class='container'>
-	<div class='row'>
-		<div id='accordion'>
-			<button class='col-md-2 col-md-offset-5 btn btn-info accordion-toggle' data-toggle='collapse' data-parent='#accordion' href='#login'>
-				Sign in
-			</button>
-			<form id='login' class='collapse col-md-1' method = 'POST' action = '/users/p_login'>
-				<input type='text' name= 'email' placeholder='Email'>
 
-				<input type = 'password' name = 'password' placeholder = 'Password'>
+			<div id='signin' class='row'>
+				<button class='col-md-2 btn btn-info accordion-toggle' data-toggle='collapse' data-parent='#signin' href='#login'>
+					Sign in
+				</button>
+				<form id='login' class='collapse col-md-1' method = 'POST' action = '/users/p_login'>
+					<input type='text' name= 'email' placeholder='Email'>
 
-				<input type='submit' class='btn btn-info' value = 'Log in' >
-			</form>
-		</div>
-	</div>
-	<div class='row'>
-		<?php if($error == "errorLogin"): ?>
-			<div class = 'col-md-3 col-md-offset-5'>
-				<p class='error'>Login Failed.  The email or password you enetered was incorrect.</p>
+					<input type = 'password' name = 'password' placeholder = 'Password'>
+
+					<input type='submit' class='btn btn-info' value = 'Log in' >
+				</form>
 			</div>
-		<?php elseif($error == "errorProtected"): ?>
-			<div class = 'col-md-3 col-md-offset-5'>
-				<p class='error'>You must be logged in to see this content</p>
-			</div>	
-		<?php endif; ?>
+		<div class='row'>
+			<?php if($error == "errorLogin"): ?>
+				<div class = 'col-md-3 col-md-offset-6'>
+					<p class='error'>Login Failed.  The email or password you enetered was incorrect.</p>
+				</div>
+			<?php elseif($error == "errorProtected"): ?>
+				<div class = 'col-md-3 col-md-offset-6'>
+					<p class='error'>You must be logged in to see this content</p>
+				</div>	
+			<?php endif; ?>
+		</div>	
 	</div>	
 </div>
